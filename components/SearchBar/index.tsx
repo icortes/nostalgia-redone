fe0b2eafa1fc3e3e350026ff487494b21fca3402
isa <5445946+icortes@users.noreply.github.com>
2022-06-29 00:22:12 -0700

    responsive app bar

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,21 +1,30 @@
-import { Tv } from '@mui/icons-material';
+import { Menu as MenuIcon, Tv } from '@mui/icons-material';
 import {
   AppBar,
   Box,
+  Button,
   Container,
   Icon,
   IconButton,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
 } from '@mui/material';
+import { MouseEvent, useState } from 'react';
 
-import { useState } from 'react';
+const pages = ['Search', 'My List'];
 
 export default function SearchBar() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileAnchorEl] = useState<null | HTMLElement>(
-    null
-  );
+
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar position='static'>
@@ -40,6 +49,63 @@ export default function SearchBar() {
             }}>
             Nostalgia Generator
           </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorEl}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+              keepMounted
+              transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+              open={Boolean(anchorEl)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign={'center'}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Tv
+            fontSize='large'
+            sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}
+          />
+          <Typography
+            variant='h5'
+            noWrap
+            component={'a'}
+            href='/'
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'Gloria Hallelujah, cursive',
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}>
+            Nostalgia Generator
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}>
+                {page}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>

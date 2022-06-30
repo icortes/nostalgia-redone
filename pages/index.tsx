4e6a3147e96948e1fdf5510d5920c2c3b9d65f50
isa <5445946+icortes@users.noreply.github.com>
2022-06-30 02:38:30 -0700

    added getserversideprops and styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,15 +7,18 @@ import {
   MenuItem,
   TextField,
 } from '@mui/material';
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import { ChangeEvent, FormEvent, useState } from 'react';
 import ResponsiveAppBar from '../components/AppBar';
-import MediaCard from '../components/MediaCard/index.';
+import MediaCard from '../components/MediaCard';
 
-const Home: NextPage = () => {
-  const [search, setSearch] = useState<Number>(2022);
-  const [media, setMedia] = useState<String>('All');
+const initialSearch = 2022;
+const initialMedia = 'All';
+
+function Home(props: any) {
+  const [search, setSearch] = useState<Number>(initialSearch);
+  const [media, setMedia] = useState<String>(initialMedia);
 
   const options = [
     { mediaType: 'All' },
@@ -54,62 +57,74 @@ const Home: NextPage = () => {
 
       <Container component={'main'} maxWidth='md'>
         <CssBaseline />
-        <Box sx={{ mt: 0 }}>
-          <Box
-            component={'form'}
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}>
-            <Grid container spacing={2} justifyContent={'center'}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  type={'number'}
-                  margin='normal'
-                  required
-                  fullWidth
-                  id='search'
-                  name='search'
-                  label='Search Year (YYYY)'
-                  value={search}
-                  autoFocus
-                  onChange={handleSearchChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  margin='normal'
-                  id='select-media'
-                  select
-                  fullWidth
-                  required
-                  label={'Select Media Type'}
-                  value={media}
-                  onChange={handleMediaChange}>
-                  {options.map((option) => (
-                    <MenuItem key={option.mediaType} value={option.mediaType}>
-                      {option.mediaType}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
-              <Grid item sx={{ mb: 1 }}>
-                <Button type='submit' variant='contained' color='secondary'>
-                  Search
-                </Button>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
 
-      <MediaCard
-        posterUrl='9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg'
-        title='Doctor Strange in the Multiverse of Madness'
-        releaseDate='2022-05-04'
-        overview='Doctor Strange, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.'
-      />
+        <Box
+          bgcolor={'white'}
+          component={'form'}
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{ my: 4, px: 3 }}>
+          <Grid container spacing={2} justifyContent={'center'}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                type={'number'}
+                margin='normal'
+                required
+                fullWidth
+                id='search'
+                name='search'
+                label='Search Year (YYYY)'
+                value={search}
+                autoFocus
+                onChange={handleSearchChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin='normal'
+                id='select-media'
+                select
+                fullWidth
+                required
+                label={'Select Media Type'}
+                value={media}
+                onChange={handleMediaChange}>
+                {options.map((option) => (
+                  <MenuItem key={option.mediaType} value={option.mediaType}>
+                    {option.mediaType}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item sx={{ mb: 1 }}>
+              <Button type='submit' variant='contained' color='secondary'>
+                Search
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <MediaCard
+          posterUrl={props.posterUrl}
+          title={props.title}
+          releaseDate={props.releaseDate}
+          overview={props.overview}
+        />
+      </Container>
     </>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {
+      posterUrl: '9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg',
+      title: 'Doctor Strange in the Multiverse of Madness',
+      releaseDate: '2022-05-04',
+      overview:
+        'Doctor Strange, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.',
+    },
+  };
 };
 
 export default Home;

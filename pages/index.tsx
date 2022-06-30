738c76641bcf5c2fe2fbeab95362be9b29f3ea98
isa <5445946+icortes@users.noreply.github.com>
2022-06-29 21:04:46 -0700

    added media type options

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,39 @@
-import { Box, Container, CssBaseline, Grid, TextField } from '@mui/material';
+import {
+  Box,
+  Container,
+  CssBaseline,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import ResponsiveAppBar from '../components/AppBar';
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState<Number>(2022);
+  const [media, setMedia] = useState<String>('All');
+
+  const options = [
+    { mediaType: 'All' },
+    { mediaType: 'Movies' },
+    { mediaType: 'TV' },
+  ];
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
       search: data.get('search'),
-      year: data.get('year'),
       media: data.get('media'),
     });
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMedia(event.target.value);
   };
 
   return (
@@ -27,7 +48,7 @@ const Home: NextPage = () => {
 
       <Container component={'main'} maxWidth='md'>
         <CssBaseline />
-        <Box sx={{ mt: 1 }}>
+        <Box sx={{ mt: 0 }}>
           <Box
             component={'form'}
             onSubmit={handleSubmit}
@@ -47,7 +68,21 @@ const Home: NextPage = () => {
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-            
+                <TextField
+                  margin='normal'
+                  id='select-media'
+                  select
+                  fullWidth
+                  required
+                  label={'Select Media Type'}
+                  value={media}
+                  onChange={handleChange}>
+                  {options.map((option) => (
+                    <MenuItem key={option.mediaType} value={option.mediaType}>
+                      {option.mediaType}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </Grid>
             </Grid>
           </Box>
